96091e31c0ac6ac1347ec2362c59db44d25a4b59
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-05-14 08:40:43 -0300

    terminando dia 2

--- index.js
+++ index.js
@@ -47,8 +47,83 @@ let temIdadeMinima = true;
 let temTituloEleitor = false;
 
 let podevotar = temIdadeMinima && temTituloEleitor;
-console.log("pode votar? " + (podevotar? "phoode" :"não pode"));
+//console.log("pode votar? " + (podevotar? "phoode" :"não pode"));
+//fato interessante: se tirar as parenteses do pode votar, a string de antes não aparece
 
 
 let podeViajar = !podevotar? "pode":"não pode";
-console.log("POde viajar? " +podeViajar )
+//console.log("POde viajar? " +podeViajar )
+
+
+let corCliente = undefined;
+let corEstoque = "Black";
+let corVendida = corCliente || corEstoque;
+//console.log(corVendida);
+//no JS é possível usar operadores lógicos com String's
+
+let driverSpeed = 110;
+
+ if (driverSpeed > 110){
+//     console.log("Driving too fast");
+ }
+ else if(driverSpeed>40 && driverSpeed<=110){
+//     console.log("Ok");
+ }
+ else{
+//     console.log("Driving too slow");
+ }
+
+
+let airport = "";
+
+switch(airport){
+    case "MCO":
+        console.log("Orlando");
+        break;
+    case "JFK":
+        console.log("John F. Kennedy");
+        break;
+    case "SEA":
+        console.log("Seattle");
+        break;
+    default:
+//        console.log("Unknown");    
+    }
+
+for(i=10;i<=20;i++){
+    if(i === 15){
+        break;
+    }
+    console.log("Número: "+i);
+}
+
+let j = 1;
+while(j<=10){
+  //  console.log("Numero"+j);
+    j++;
+}
+
+let k=1;
+do{
+  //  console.log(k);
+    k++;
+}while(k<=10);
+
+
+const myCar={
+    model: "BMW",
+    year:2023,
+    km:68000
+}
+//for each gambiarra
+for( let i in myCar){
+    // console.log(i, myCar[i]);
+};
+
+
+//for each de verdade
+const amigos = ["Marcos", "ana", "Jose"];
+
+for (let i of amigos){
+ //   console.log( i );
+}
