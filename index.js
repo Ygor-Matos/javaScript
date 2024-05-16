a6f4feff7421f7879afe79dd3d285ed797b05c8d
Ygor Matos <ygorsilvamatos@hotmail.com>
2024-05-16 09:02:02 -0300

    concluindo o dia 4'

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ pen.itemColor = "blue";
 
 //exemplo de array
 
-let friends = ['Marcos',"Silvia","Elisa"];
-friends[1]="Junior";
+let friends3 = ['Marcos',"Silvia","Elisa"];
+friends3[1]="Junior";
 //console.log(friends);
 
 function saleStatus(status, total ) {
@@ -147,7 +147,7 @@ function createBook(title, autor, pages, chapters){
 let book1= createBook("principe da persia","Ubisoft", 500);
 let book2= createBook("Harry Potter","J.K Rowling", 200);
 book1.color="Red";
-console.log(book1);
+// console.log(book1);
 
 
 //fazendo um constructor
@@ -164,15 +164,80 @@ function MusicFile(musica, cantor, disco, duracao){
 }
 
 let musicaTeste= new MusicFile('fogueira',"Felipe Amorim", "Ao After e Além", 2.12);
-console.log(musicaTeste);
+// console.log(musicaTeste);
 // musicaTeste.play();
 
 
-//template literals
-let nome = "thiago"
-const email = 'Hi '+nome+'\nThe meeting is confirmerd! \nAndre';
-console.log(email);
-const email2 =`Hi ${nome},
-The meeting is confirmed!
-Ygor`
-console.log(email2);
+// //template literals
+// let nome = "thiago"
+// const email = 'Hi '+nome+'\nThe meeting is confirmerd! \nAndre';
+// console.log(email);
+// const email2 =`Hi ${nome},
+// The meeting is confirmed!
+// Ygor`;
+// console.log(email2);
+
+const friends = ['marcos', 'Ana','Marcela'];
+const num = [7,8,9];
+// num.push(10,11,12);
+// num.unshift(1,2,3);
+// num.splice(3,0,4,5,6);
+console.log(num);
+
+
+console.log(num.indexOf(8));
+
+const movies = [
+    {id:1, movieName: 'Dejavu'},
+    {id:2, movieName: 'Back to the Future'},
+    {id:3, movieName: 'The Matrix'}
+]
+
+//em casos onde o objeto não é do tipo primitivo, ao invés de includes usarei a função find
+console.log(movies.find(function(movie){
+    //return movie.movieName == 'The Matrix'
+}));
+
+
+//usando arrowfunction
+//console.log(movies.find(movie => movie.movieName == 'Back to the Future'));
+
+let num5 = [5,6,7,8];
+//const final = num5.splice(2,2);
+//pop deleta o último
+//shift remove o primeiro
+console.log(num5);
+//console.log(final);
+
+//esvaziando um array
+
+// num5 =[];
+// num5.length = 0;
+// num.splice(0, num5.length);
+
+let letters = ['a','b','c'];
+all = num5.concat(letters);
+half = all.slice(3,5);
+
+console.log(all);
+console.log(half);
+
+
+let clients = [ 'jose','Marcela', 'Andre'];
+
+console.log(clients);
+
+let clientsJoin = clients.join(', ');
+
+console.log(clientsJoin);
+//clients.sort();
+//clients.reverse();
+console.log(clients);  
+
+const tempLondon = [18,-13,8,-2,-9];
+
+//const tempPositive = tempLondon.every (negative => negative>=0);
+
+const tempPositive = tempLondon.filter(negative => negative>=0);
+
+console.log(tempPositive);
